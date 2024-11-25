47e297def048a96b18b21350041025bc507ffdab
DonRom4ntico <angelojamescoronadoasher@gmail.com>
2024-11-25 14:17:49 +0800

    11

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarlBakerzShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CarlBakerzShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarlBakerzShop.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
